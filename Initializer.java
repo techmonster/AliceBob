e34b59a53ee63df578d90b078bd89c38f4a254cd
nathanielholloway <n8talent@gmail.com>
2016-09-12 05:04:37 -0400

    Final version

---
+++ Initializer.java
@@ -0,0 +1,13 @@
+package holloway.nate.AliceBob;
+
+/**
+ * Created by nathanielholloway on 9/10/16.
+ * This program returns a greeting.
+ */
+public class Initializer {
+    public static void main(String[] args) {
+        Compute start = new Compute();
+        start.logic();
+    }
+
+}
